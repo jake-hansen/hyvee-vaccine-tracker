39cc3397e606d38f62cafe2e0f03627c205fa820
Jake Hansen <jakedhansen@outlook.com>
2021-04-03 12:25:46 -0500

    Add domain representations of Pharmacy and Address
    Add adapters from hyvee api domain to application domain.

--- domain/pharmacy.go
+++ domain/pharmacy.go
@@ -1,2 +1,18 @@
 package domain
 
+type PhoneNumber string
+
+type Pharmacy struct {
+	Name string
+	Address Address
+	PhoneNumber PhoneNumber
+	VaccinationsAvailable	bool
+}
+
+type Address struct {
+	Line1	string
+	Line2	string
+	City	string
+	State	string
+	Zip		int
+}
